33b48e64512064da03c8eb619c5ac2afe0f2d757
Chris Collins <kuroneko@sysadninjas.net>
2020-04-12 21:12:19 +1000

    update the keyboard mapping to include the entry for KeyPadEnter

--- ImgWindow.cpp
+++ ImgWindow.cpp
@@ -66,8 +66,9 @@ ImgWindow::ImgWindow(
 	io.KeyMap[ImGuiKey_Delete] = XPLM_VK_DELETE;
 	io.KeyMap[ImGuiKey_Backspace] = XPLM_VK_BACK;
 	io.KeyMap[ImGuiKey_Space] = XPLM_VK_SPACE;
-	io.KeyMap[ImGuiKey_Enter] = XPLM_VK_ENTER;
-	io.KeyMap[ImGuiKey_Escape] = XPLM_VK_ESCAPE;
+    io.KeyMap[ImGuiKey_Enter] = XPLM_VK_RETURN;
+    io.KeyMap[ImGuiKey_Escape] = XPLM_VK_ESCAPE;
+    io.KeyMap[ImGuiKey_KeyPadEnter] = XPLM_VK_ENTER;
 	io.KeyMap[ImGuiKey_A] = XPLM_VK_A;
 	io.KeyMap[ImGuiKey_C] = XPLM_VK_C;
 	io.KeyMap[ImGuiKey_V] = XPLM_VK_V;
